27eebc6d4b110758addd85a9fe9be7f7fbcc585d
Ryan & Jason Lane <student@epicodus.com>
2015-08-10 09:51:22 -0700

    Add tests for equilateral triangles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,15 @@ class Triangle
   end
 
   define_method(:isTriangle?) do
-    @side_a != 0 && @side_b != 0 && @side_c != 0
+    sides = (@side_a != 0 && @side_b != 0 && @side_c != 0)
+
+    sides_a_b = (@side_a + @side_b) >= @side_c
+    sides_b_c = (@side_b + @side_c) >= @side_a
+    sides_c_a = (@side_c + @side_a) >= @side_b
+
+    sides && sides_a_b && sides_b_c && sides_c_a
+       
   end
-  
+
+
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,13 +2,28 @@ require 'rspec'
 require './lib/triangle'
 
 describe "Triangle" do
-  describe "Triangle.isTriangle?" do
-    it 'if it has three sides returns true' do
+  describe "Triangle#isTriangle?" do
+    it 'has three sides returns true' do
       expect(Triangle.new(3,3,3).isTriangle?).to eq(true)
     end
 
-    it 'if it doesnt have three sides returns false' do
+    it 'doesnt have three sides returns false' do
       expect(Triangle.new(3, 3, 0).isTriangle?).to eq(false)
     end
+
+    it 'the sum of any two sides are greater than the third side, return false' do
+      expect(Triangle.new(3, 3, 7).isTriangle?).to eq(false)
+    end
   end
+
+  describe "Triangle#isEqualateral?" do
+    it 'returns true if all the sides are equal' do
+      expect(Triangle.new(3, 3, 3).isEqualateral?).to eq(true)
+    end
+
+    it 'returns false if all the sides are not equal' do
+      expect(Triangle.new(4, 4, 5).isEqualateral?).to eq(false)
+    end
+
+
 end
